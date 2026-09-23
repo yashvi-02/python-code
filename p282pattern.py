3dd209555a9009cdb095c3a5163e6f39d5647343
Yashvi Ghaghda <yashvighaghda02@gmail.com>
2026-09-23 17:17:03 +0530

    Add files via upload

---
+++ p282pattern.py
@@ -0,0 +1,7 @@
+num = int(input("Enter number: "))
+
+for i in range(num):
+    for j in range(num):
+        print("*", end=" ")
+    print("*")
+    
---
+++ p287pattern.py
@@ -0,0 +1,5 @@
+num = int(input("Enter number: "))
+for i in range(1,4):
+    for j in range(1,num+1):
+        print("*", end = " ")
+    print(" ")
